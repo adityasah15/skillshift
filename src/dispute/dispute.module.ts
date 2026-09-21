b8801eaf5cf9ca31b68066cc63e8600d002873d8
Aditya Sah <adityasah0156@gmail.com>
2026-09-21 13:54:53 +0000

    fix: wire dispute module dependencies

--- src/dispute/dispute.module.ts
+++ src/dispute/dispute.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DisputeService } from './dispute.service';
 import { DisputeController } from './dispute.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { NotificationModule } from 'src/notification/notification.module';
 
 @Module({
+  imports: [PrismaModule, NotificationModule],
   controllers: [DisputeController],
   providers: [DisputeService],
 })
